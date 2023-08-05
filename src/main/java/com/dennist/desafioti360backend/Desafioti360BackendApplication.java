5176eb7281cd9ee11197a8640000ad8eddc78e9f
Dennis Teles <dennis.teles@gmail.com>
2023-08-04 23:44:21 -0300

    Adicionando primeiros arquivos

---
+++ src/main/java/com/dennist/desafioti360backend/Desafioti360BackendApplication.java
@@ -0,0 +1,13 @@
+package com.dennist.desafioti360backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Desafioti360BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Desafioti360BackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/dennist/desafioti360backend/Desafioti360BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dennist.desafioti360backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Desafioti360BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
