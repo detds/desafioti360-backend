9119f2dac0a1b5de2dcdfa6393c76b667297cac5
Dennis Teles <dennis.teles@gmail.com>
2023-08-07 00:11:59 -0300

    Implementando Curso (model, controller, service e repository) e findAll

---
+++ src/main/java/com/dennist/desafioti360backend/models/Curso.java
@@ -0,0 +1,55 @@
+package com.dennist.desafioti360backend.models;
+
+import jakarta.persistence.*;
+
+import java.io.Serial;
+import java.io.Serializable;
+import java.util.Objects;
+
+@Entity
+@Table(name = "tb_curso")
+public class Curso implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long codigo;
+    private String nome;
+
+    public Curso() {
+    }
+
+    public Curso(Long codigo, String nome) {
+        this.codigo = codigo;
+        this.nome = nome;
+    }
+
+    public Long getCodigo() {
+        return codigo;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Curso curso = (Curso) o;
+
+        return Objects.equals(codigo, curso.codigo);
+    }
+
+    @Override
+    public int hashCode() {
+        return codigo != null ? codigo.hashCode() : 0;
+    }
+}
