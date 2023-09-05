c2c2cae7562be4cd8f7069e8a64f6cc3e254a925
Dennis Teles <dennis.teles@gmail.com>
2023-09-05 20:51:20 -0300

    :sparkles: feat: Implementado healthCheck
    Adicionado endpoint /health que retorna como resposta se a aplicação está funcionando.

---
+++ src/main/java/com/dennist/desafioti360backend/controllers/HealthCheckController.java
@@ -0,0 +1,17 @@
+package com.dennist.desafioti360backend.controllers;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/health")
+public class HealthCheckController {
+
+    @GetMapping
+    public ResponseEntity<String> healthCheck() {
+        return ResponseEntity.ok("A aplicação está funcionando!");
+    }
+
+}
