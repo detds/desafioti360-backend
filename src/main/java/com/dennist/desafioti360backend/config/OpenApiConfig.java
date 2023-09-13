1c67ed3c30b5d6f4e1d602fb044a587e1c9e44af
Dennis Teles <dennis.teles@gmail.com>
2023-09-12 23:05:59 -0300

    :bug: fix: Corrigido endereço da aplicação no Swagger

--- src/main/java/com/dennist/desafioti360backend/config/OpenApiConfig.java
+++ src/main/java/com/dennist/desafioti360backend/config/OpenApiConfig.java
@@ -2,8 +2,10 @@ package com.dennist.desafioti360backend.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
 import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.servers.Server;
 
-@OpenAPIDefinition(info = @Info(
+@OpenAPIDefinition(servers = @Server(url = "https://desafio-backend-api.fly.dev"),
+        info = @Info(
         description = "Microsserviço para Cadastro e Consulta de Alunos e Cursos",
         title = "Desafio TI360 - Backend API",
         version = "1.0"))
